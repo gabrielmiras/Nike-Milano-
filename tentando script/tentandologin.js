738bce6b8759047f1607cbb2a7f0ee1eb0694627
gabrielmiras <gabrielznrg@gmail.com>
2026-04-22 14:49:53 -0300

    Ultima atividade Bootstrap

--- tentando script/tentandologin.js
+++ tentando script/tentandologin.js
@@ -2,19 +2,23 @@ const prism = document.getElementById("prism");
 
 function movePrism(face) {
     const rotations = {
-        'front':  'translateZ(-190px) rotateY(0deg)',
-        'right':  'translateZ(-190px) rotateY(-90deg)',
-        'back':   'translateZ(-190px) rotateY(-180deg)',
-        'left':   'translateZ(-190px) rotateY(90deg)',
-        'bottom': 'translateZ(-190px) rotateX(90deg)'
+        'front':  'translateZ(-200px) rotateY(0deg)',
+        'right':  'translateZ(-200px) rotateY(-90deg)',
+        'back':   'translateZ(-200px) rotateY(-180deg)',
+        'left':   'translateZ(-200px) rotateY(90deg)',
+        'bottom': 'translateZ(-200px) rotateX(90deg)'
     };
     
     prism.style.transform = rotations[face];
 }
 
 function showSuccess() {
-    // Simula um delay de processamento
+
+    const activeBtn = document.querySelector('.btn-nike');
+    activeBtn.innerText = "WAITING...";
+    
     setTimeout(() => {
         movePrism('bottom');
-    }, 500);
+        activeBtn.innerText = "SIGN IN"; 
+    }, 800);
 }
